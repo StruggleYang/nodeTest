074ca1fcafe92d7356f259e7d1ea8d1d832ba51d
StruggleYang <yq1724555319@gmail.com>
2018-09-15 00:29:44 +0800

    npm init

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 // 启用严格模式
 'use strict'
 // 导入模块hello
-var hello = require('./hello')
-var fsHello = require('./fsHello')
+var hello = require('./modules/hello')
+var fsHello = require('./modules/fsHello')
 var s = 'Michael';
 // 调用模块hello中的greet方法
 hello.greet(s);
 hello.greet2(s);
 
 // fs 模块
-fsHello.fsReadFileSys('./static/test.txt');
+fsHello.fsReadFile('./static/test.txt');
 fsHello.fsReadFileSync('./static/test.txt');
 fsHello.writeFile("./out/out.txt",'Hello, Node.js');
 fsHello.writeFileSync("./out/out1.txt",'sync Hello, Node.js');
 fsHello.stat("./out/out1.txt");
+fsHello.statSync("./out/out1.txt");
 
 
 
