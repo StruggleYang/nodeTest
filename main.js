1057426750a03a7260014001a8c3b473bdcbdcb9
StruggleYang <yq1724555319@gmail.com>
2018-09-16 12:28:32 +0800

    add readme.md

--- main.js
+++ main.js
@@ -3,34 +3,43 @@
 // 导入模块hello
 var hello = require('./modules/hello')
 var fsHello = require('./modules/fsHello')
-var s = 'Michael';
-// 调用模块hello中的greet方法
-hello.greet(s);
-hello.greet2(s);
 
-// fs 模块
-fsHello.fsReadFile('./static/test.txt');
-fsHello.fsReadFileSync('./static/test.txt');
-fsHello.writeFile("./out/out.txt",'Hello, Node.js');
-fsHello.writeFileSync("./out/out1.txt",'sync Hello, Node.js');
-fsHello.stat("./out/out1.txt");
-fsHello.statSync("./out/out1.txt");
+function mainTest() {
+    var s = 'Michael';
+    // 调用模块hello中的greet方法
+    hello.greet(s);
+    hello.greet2(s);
+
+    // fs 模块
+    fsHello.fsReadFile('./static/test.txt');
+    fsHello.fsReadFileSync('./static/test.txt');
+    fsHello.writeFile("./out/out.txt", 'Hello, Node.js');
+    fsHello.writeFileSync("./out/out1.txt", 'sync Hello, Node.js');
+    fsHello.stat("./out/out1.txt");
+    fsHello.statSync("./out/out1.txt");
 
 
 
-// 程序即将退出时的回调函数:
-process.on('exit', function (code) {
-    console.log('about to exit with code: ' + code);
-});
-// process.nextTick()将在下一轮事件循环中调用:
-process.nextTick(function () {
-    console.log('nextTick callback!');
-});
-console.log('nextTick was set!');
+    // 程序即将退出时的回调函数:
+    process.on('exit', function (code) {
+        console.log('about to exit with code: ' + code);
+    });
+    // process.nextTick()将在下一轮事件循环中调用:
+    process.nextTick(function () {
+        console.log('nextTick callback!');
+    });
+    console.log('nextTick was set!');
 
-// 判断js运行环境
-if (typeof(window) === 'undefined') {
-    console.log('node.js');
-} else {
-    console.log('browser');
+    // 判断js运行环境
+    if (typeof (window) === 'undefined') {
+        console.log('node.js');
+    } else {
+        console.log('browser');
+    }
+}
+
+module.exports = {
+    hello: hello,
+    fsHello: fsHello,
+    mainTest: mainTest
 }
